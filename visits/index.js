6ebacf273b40bc5f7573aef1f0deecb7592414dd
Vashist <NAVA@EUR.AD.SAG>
2018-10-21 01:38:35 +0530

    added restart policy

--- visits/index.js
+++ visits/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const redis= require('redis');
+const process= require('process');
 
 const app = new express();
 const client = new redis.createClient({
@@ -12,6 +13,11 @@ app.get('/',(req,res)=>{
         res.send('Number of visits are '+ visits); 
         client.set('visits',parseInt(visits)+1);
     });
+});
+
+app.get('/crash',(req,res)=>{
+    console.log('system crashed');
+    process.exit(0);
 });
 
 app.listen(8081,()=>{
