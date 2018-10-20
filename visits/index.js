ed77702e1fd2e4e73a4f1eab8c2dc906dc913852
Vashist <NAVA@EUR.AD.SAG>
2018-10-20 23:40:34 +0530

    adding initial projects

---
+++ visits/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const redis= require('redis');
+
+const app = new express();
+const client = new redis.createClient();
+client.set('visits',0);
+app.get('/',(req,res)=>{
+    client.get('visits',(err,visits)=>{
+        res.send('Number of visits are '+ visits); 
+        client.set('visits',parseInt(visits)+1);
+    });
+});
+
+app.listen(8081,()=>{
+    console.log('Listning on port 8081');
+});
